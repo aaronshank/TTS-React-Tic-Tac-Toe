73cf997b01ee8fef4dde4a0691bce070f696f2f1
Aaron Shank <shank@ctwo.net>
2021-12-16 13:55:31 -0500

    finished game

--- src/index.js
+++ src/index.js
@@ -121,8 +121,6 @@ class Game extends React.Component {
 	}
 }
 
-// ========================================
-
 ReactDOM.render(<Game />, document.getElementById('root'))
 
 function calculateWinner(squares) {
@@ -136,11 +134,13 @@ function calculateWinner(squares) {
 		[0, 4, 8],
 		[2, 4, 6],
 	]
+
 	for (let i = 0; i < lines.length; i++) {
 		const [a, b, c] = lines[i]
 		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
 			return squares[a]
 		}
 	}
+
 	return null
 }
